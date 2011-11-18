289f21613424319f11a2266b203e48dc53bf986c
uberj <uberj@youremail.com>
2011-11-17 19:19:59 -0800

    wrote script to generate the write up.

---
+++ gen_report.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# call do_homework()
+
+do_homework(){
+    for i in 1 2 3 4 5 6 7 8 9
+    do
+        python nqueens.py "$i"
+    done
+}
+
+speak_to_ta(){
+    echo "Hello Nels (White Knight) Oscar."
+    echo "This is a \"concise report\"."
+    echo "============================="
+}
+
+explain_homework(){
+    echo "What follows is the number of unique solutions to the N-Queens problem calculated"
+    echo "for a N X N board. N starts at 1 and goes up to 9."
+    echo
+    echo "Enjoy the show!"
+    echo
+}
+
+speak_to_ta
+explain_homework
+do_homework
+
